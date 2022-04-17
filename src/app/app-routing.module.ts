cca51e26302947bd84be1c6c551dcd6e7dbf6db7
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-04-17 11:13:05 +0400

    layout readjusted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactFormularComponent } from './contact-formular/contact-formular.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillSheetComponent } from './skill-sheet/skill-sheet.component';
 import { SlidehsowComponent } from './slidehsow/slidehsow.component';
 
 const routes: Routes = [
-  { path: '', component: SlidehsowComponent },
+  { path: '', component: SlidehsowComponent},
+  { path: 'skills', component: SkillSheetComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactFormularComponent },
   { path: 'imprint', component: ImprintComponent },
   { path: 'data-protection', component: DataProtectionComponent }
 ];
