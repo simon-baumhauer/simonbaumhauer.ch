38937638a2a085bc3f8b7b03e39e6bdf9905aeaf
simon baumhauer <simon.baumhauer@hotmail.ch>
2022-04-27 07:40:22 +0200

    project selcetion function added

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -10,6 +11,38 @@ export class ProjectsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  all = true;
+  angular = true;
+  javascript = true;
+ 
+  showAll() {
+    this.all = true;
+    this.angular = true;
+    this.javascript = true;
+    document.getElementById('showAngular').classList.remove('button-active')
+    document.getElementById('showJavascript').classList.remove('button-active')
+    document.getElementById('showAll').classList.add('button-active')
+
+  }
+
+  showJavascript() {
+    this.all = false;
+    this.angular = false;
+    this.javascript = true;
+    document.getElementById('showAll').classList.remove('button-active')
+    document.getElementById('showAngular').classList.remove('button-active')
+    document.getElementById('showJavascript').classList.add('button-active')
+  }
+
+  showAngular() {
+    this.all = false;
+    this.angular = true;
+    this.javascript = false;
+    document.getElementById('showAll').classList.remove('button-active')
+    document.getElementById('showJavascript').classList.remove('button-active')
+    document.getElementById('showAngular').classList.add('button-active')
   }
 
 }
